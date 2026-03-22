02f46ef345b160ab5b507b66a9f6419b5776075d
Rafael Ramirez <rafarangeles@exalumno.unam.mx>
2026-03-22 10:53:51 -0600

    Hito alcanzado: Validación de Máquina de Estados e Integridad de Flujo (Error 500 controlado: requiere CERTIFICADO)

---
+++ benchmarks/workload/titularAlumno.js
@@ -0,0 +1,33 @@
+﻿'use strict';
+
+const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
+
+class MyWorkload extends WorkloadModuleBase {
+    constructor() {
+        super();
+        this.txIndex = 0;
+    }
+
+    async submitTransaction() {
+        this.txIndex++;
+        // Caliper usa workerIndex para separar el trabajo entre sus hilos de ejecución
+        const alumnoID = `ALUMNO${this.workerIndex}_${this.txIndex}`;
+        const hashTitulo = `HASH_TITULO_AUDITADO_${this.txIndex}`;
+
+        const request = {
+            contractId: 'uacm-contract',
+            contractFunction: 'TitularAlumno',
+            invokerIdentity: '_Org2MSP_User1',
+            contractArguments: [alumnoID, hashTitulo],
+            readOnly: false
+        };
+
+        await this.sutAdapter.sendRequests(request);
+    }
+}
+
+function createWorkloadModule() {
+    return new MyWorkload();
+}
+
+module.exports.createWorkloadModule = createWorkloadModule;
