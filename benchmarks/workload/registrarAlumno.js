c0c5064843eb3aa42753f15f5907ff9c6b5220a8
Rafael Ramirez <rafarangeles@exalumno.unam.mx>
2026-03-21 19:37:22 -0600

    HITO: Implementación de v1.4 con validación de integridad y primer benchmark exitoso (5 TPS)

---
+++ benchmarks/workload/registrarAlumno.js
@@ -0,0 +1,32 @@
+﻿'use strict';
+
+const { WorkloadModuleBase } = require('@hyperledger/caliper-core');
+
+class MyWorkload extends WorkloadModuleBase {
+    constructor() {
+        super();
+        this.txIndex = 0;
+    }
+
+    async submitTransaction() {
+        this.txIndex++;
+        const alumnoID = `ALUMNO_${this.workerIndex}_${this.txIndex}`;
+        const nombre = `Estudiante Prueba ${this.txIndex}`;
+
+        const request = {
+            contractId: 'uacm-contract',
+            contractFunction: 'RegistrarIngreso',
+            invokerIdentity: 'User1',
+            contractArguments: [alumnoID, nombre],
+            readOnly: false
+        };
+
+        await this.sutAdapter.sendRequests(request);
+    }
+}
+
+function createWorkloadModule() {
+    return new MyWorkload();
+}
+
+module.exports.createWorkloadModule = createWorkloadModule;
